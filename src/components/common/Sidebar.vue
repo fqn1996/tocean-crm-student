d212cdc2dfe0985e67f49c2e3a3cf15c302c96a5
Fqn1996 <976754760@qq.com>
2017-11-01 14:01:19 +0800

    Initial commit

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,147 @@
+<template>
+    <div class="sidebar">
+        <el-menu :default-active="onRoutes" class="el-menu-vertical-demo" theme="dark" unique-opened router>
+            <template v-for="item in items">
+                <template v-if="item.subs">
+                    <el-submenu :index="item.index">
+                        <template slot="title"><i :class="item.icon"></i>{{ item.title }}</template>
+                        <el-menu-item v-for="(subItem,i) in item.subs" :key="i" :index="subItem.index">{{ subItem.title }}
+                        </el-menu-item>
+                    </el-submenu>
+                </template>
+                <template v-else>
+                    <el-menu-item :index="item.index">
+                        <i :class="item.icon"></i>{{ item.title }}
+                    </el-menu-item>
+                </template>
+            </template>
+        </el-menu>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                items: [
+                    {
+                        icon: 'el-icon-setting',
+                        index: 'readme',
+                        title: '提交作业'
+                    },
+                    {
+                        icon: 'el-icon-menu',
+                        index: '2',
+                        title: '提交报告',
+                        subs: [
+                            {
+                                index: 'basetable',
+                                title: '提交日报'
+                            },
+                            {
+                                index: 'vuetable',
+                                title: '提交周报'
+                            },
+                            {
+                                index: 'historyday',
+                                title: '历史日报'
+                            },
+                            {
+                                index: 'historyweek',
+                                title: '历史周报'
+                            }
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-date',
+                        index: '3',
+                        title: '问卷调查',
+                        subs: [
+                            {
+                                index: 'baseform',
+                                title: '填写问卷'
+                            },
+                            {
+                                index: 'vueeditor',
+                                title: '历史问卷'
+                            },
+                        ]
+                    },
+                    {
+                        icon: 'el-icon-star-on',
+                        index: 'basecharts',
+                        title: '上传简历'
+                    },
+                    {
+                        icon: 'el-icon-date',
+                        index: '4',
+                        title: '学员信息查询',
+                        subs: [
+                            {
+                                index: 'basetable',
+                                title: '学员档案'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '考勤记录'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '违规记录'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '表扬记录'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '作业完成情况'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '项目考勤情况'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '班级管理积极性'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '演讲查询'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '分组讨论查询'
+                            },
+                            {
+                                index: 'basetable',
+                                title: '综合考评分'
+                            },
+
+                        ]
+                    }
+                ]
+            }
+        },
+        computed:{
+            onRoutes(){
+                return this.$route.path.replace('/','');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .sidebar{
+        display: block;
+        position: absolute;
+        width: 250px;
+        left: 0;
+        top: 70px;
+        bottom:0;
+        background: #2E363F;
+    }
+    .sidebar > ul {
+        height:100%;
+    }
+</style>
