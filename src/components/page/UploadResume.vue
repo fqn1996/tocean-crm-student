33154eeaf635bfc594c46f24dba05da7b50c321c
huangpn <1715009306@qq.com>
2017-11-02 11:34:15 +0800

    修改了UploadResume.vue（hpn）

--- src/components/page/UploadResume.vue
+++ src/components/page/UploadResume.vue
@@ -2,19 +2,60 @@
     <div class="ur_content">
     <div class="ur_header">
       当前功能:&nbsp;&nbsp;<b>上传简介信息</b>
-    </div>
+       </div>
         <div class="ur_header2">
-            <button id="ur_tj" >提交</button>
+            <el-button size="small" id="ur_tj" >提交</el-button>
         </div>
-        <div class="ur_table">
-            <el-table border style="width: 100%"  >
-                <el-table-column  label="基本资料 (+ 单击标题栏展开)" >
-                </el-table-column>
-            </el-table>
+        <div class="ur_table" >
+            <el-collapse  v-model="activeNames" @change="handleChange">
+                <el-collapse-item title="基本资料 (+ 单击标题栏展开) " name="1">
+                    简历:
+                    <el-upload
+                    class="upload-demo"
+                    ref="upload"
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :on-preview="handlePreview"
+                    :on-remove="handleRemove"
+                    :file-list="fileList"
+                    :auto-upload="false">
+                    <div slot="trigger"  type="primary" class="el-upload__text">
+                        <i class="el-icon-upload"></i>将文件拖到此处，或<em>点击上传</em></div>
+                    <el-button  style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+                        <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb
+                    </div>
+                </el-upload>
+
+                </el-collapse-item>
+            </el-collapse>
+        </div>
+        <div class="ur_footer">
+            <el-button id="qd">确定提交</el-button>
         </div>
     </div>
+
 </template>
-<script>
+<script >
+    import ElInput from "../../../node_modules/element-ui/packages/input/src/input";
+    export default {
+        components: {ElInput}, data() {
+            return {
+                activeNames: ['1'],
+                fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'},
+                    {name: 'food2.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}]
+            };
+        },
+        methods: {
+            submitUpload() {
+                this.$refs.upload.submit();
+            },
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            }
+        }
+    }
 </script>
 <style>
 .ur_header{
@@ -31,6 +72,15 @@
     padding: 5px 0;
     margin-bottom: 20px;
     height: 30px;
+}
+.ur_footer{
+    background-color:#EEF1F6;
+    border: solid  1px #DFE6EC;
+    color:black;
+    padding: 5px 0;
+    margin-top: 20px;
+    height: 30px;
+    text-align: center;
 }
     #ur_tj{
         float: right;
